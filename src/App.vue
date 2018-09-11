38c06476b1aa58315c4fb174bb967c597864ecca
Lem92 <mbaylem92@gmail.com>
2018-09-11 20:17:00 +0300

    last commit

--- src/App.vue
+++ src/App.vue
@@ -76,60 +76,11 @@
 </template>
 
 <script>
+    import Main from './mixin/main.js'
     export default {
         name: 'app',
-        data () {
-            return {
-                data: [],
-                activeCurrency: 'USD',
-                currencies: ['USD', 'EUR', 'RUB', 'GBP'],
-                cryptoCurrencies: [
-                    {
-                        value: 'ETH',
-                        name: 'Ethereum',
-                        isPercentChange: false,
-                    },
-                    {
-                        value: 'LTC',
-                        name: 'Litecoin',
-                        isPercentChange: false,
-                    },
-                    {
-                        value: 'BTC',
-                        name: 'Bitcoin',
-                        isPercentChange: false,
-                    },
-                ],
-                isDropDownShow: true,
-            }
-        },
-        methods: {
-            fetchData(cryptoCurrency, activeCurrency) {
-                fetch(`https://apiv2.bitcoinaverage.com/indices/global/ticker/${cryptoCurrency.value}${activeCurrency}`, {
-                    method: 'GET'
-                }).then(response => response.json())
-                    .then(json => this.data.push({json, cryptoCurrency}));
-            },
-            selectActiveCurrency(currency) {
-                this.data = [];
-                this.toggleDropdown();
-                this.activeCurrency = currency;
-                this.cryptoCurrencies.forEach((item) => {
-                    this.fetchData(item, this.activeCurrency);
-                });
-            },
-            toggleDropdown() {
-                this.isDropDownShow = !this.isDropDownShow;
-            },
-            getValue(isPercentChange, item, valueKey) {
-                return {
-                    value: isPercentChange ? `${item.percent[valueKey]}%` : `${item.price[valueKey]}$`,
-                    sign: parseFloat(isPercentChange ? `${item.percent[valueKey]}%` : `${item.price[valueKey]}$`) > 0
-                }
-            },
-        },
-        created () {
-            this.selectActiveCurrency(this.activeCurrency);
-        },
+        mixins: [
+            Main,
+        ],
     }
 </script>
--- src/mixin/main.js
+++ src/mixin/main.js
@@ -1,23 +1,55 @@
 export default {
     data () {
         return {
-            data: {
-                data: {},
-                activeCurrency: 'usd',
-                currency: ['usd', 'eur', 'rub', 'gbp'],
-            },
+            data: [],
+            activeCurrency: 'USD',
+            currencies: ['USD', 'EUR', 'RUB', 'GBP'],
+            cryptoCurrencies: [
+                {
+                    value: 'ETH',
+                    name: 'Ethereum',
+                    isPercentChange: false,
+                },
+                {
+                    value: 'LTC',
+                    name: 'Litecoin',
+                    isPercentChange: false,
+                },
+                {
+                    value: 'BTC',
+                    name: 'Bitcoin',
+                    isPercentChange: false,
+                },
+            ],
+            isDropDownShow: true,
         }
     },
     methods: {
-        fetchData() {
-            fetch(`https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD`, {
+        fetchData(cryptoCurrency, activeCurrency) {
+            fetch(`https://apiv2.bitcoinaverage.com/indices/global/ticker/${cryptoCurrency.value}${activeCurrency}`, {
                 method: 'GET'
-            }).then(
-                response => response.json())
-                .then(json => this.data = json)
-        }
+            }).then(response => response.json())
+                .then(json => this.data.push({json, cryptoCurrency}));
+        },
+        selectActiveCurrency(currency) {
+            this.data = [];
+            this.toggleDropdown();
+            this.activeCurrency = currency;
+            this.cryptoCurrencies.forEach((item) => {
+                this.fetchData(item, this.activeCurrency);
+            });
+        },
+        toggleDropdown() {
+            this.isDropDownShow = !this.isDropDownShow;
+        },
+        getValue(isPercentChange, item, valueKey) {
+            return {
+                value: isPercentChange ? `${item.percent[valueKey]}%` : `${item.price[valueKey]}$`,
+                sign: parseFloat(isPercentChange ? `${item.percent[valueKey]}%` : `${item.price[valueKey]}$`) > 0
+            }
+        },
     },
-    created() {
-        this.fetchData();
+    created () {
+        this.selectActiveCurrency(this.activeCurrency);
     },
 }
